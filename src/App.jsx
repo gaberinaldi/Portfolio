dc50d29ce4dbd836acb82555e036515bf8914f9a
Gabriel Rinaldi <gaberinaldi.bio@gmail.com>
2023-11-24 21:30:09 -0700

    added-styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
 import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <>
-      <Header />
+      
       <NavBar />
       <main className='container'>
       <Outlet />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 export default function Home() {
     return (
-        <div>
-            <p>This is where ill talk about myself</p>
+        <div className="about">
+            <div className="homeText">
+            <h1>Hello there,</h1>
+            <div className="typewriter">
+            <h1 className="effect">my name is <span className="firstName">Gabriel</span> Rinaldi</h1>
+            </div>
+            <p>
+                I'm a motivated 21 year old Front-End Developer based in Salt Lake City, UT. &#128205;
+            </p>
+            </div>
+            <div className="profile-pic">
+                <img src="src/images/avi.png" alt="Gabriel Rinaldi" />
+            </div>
         </div>
     );
 };
